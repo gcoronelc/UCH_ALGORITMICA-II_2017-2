97623717416881256ed25989f22d9d66181a9d6d
Gustavo Coronel <gcoronelc@gmail.com>
2017-10-06 22:02:41 -0500

    Sem 06

--- Sem06/PedidoApp/src/pe/uch/pedidoapp/PedidoApp.java
+++ Sem06/PedidoApp/src/pe/uch/pedidoapp/PedidoApp.java
@@ -1,9 +1,11 @@
 package pe.uch.pedidoapp;
 
+import pe.uch.pedidoapp.view.PedidoView;
+
 public class PedidoApp {
 
     public static void main(String[] args) {
-
+        PedidoView.main(args);
     }
     
 }
