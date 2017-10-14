6908dd4bfe281399d78617ed99e4727a6eadd715
Docente UCH <docente@soporteuch.pe>
2017-10-13 19:39:48 -0500

    Sem 07

--- Sem07/PrecioVentaApp/src/pe/uch/precioventaapp/PrecioVentaApp.java
+++ Sem07/PrecioVentaApp/src/pe/uch/precioventaapp/PrecioVentaApp.java
@@ -1,8 +1,11 @@
 package pe.uch.precioventaapp;
 
+import pe.uch.precioventaapp.view.PrecioVentaView;
+
 public class PrecioVentaApp {
 
     public static void main(String[] args) {
+        PrecioVentaView.main(args);
     }
     
 }
